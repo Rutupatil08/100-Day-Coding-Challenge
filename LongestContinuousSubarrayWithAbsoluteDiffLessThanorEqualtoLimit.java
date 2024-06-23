5670a1f98bb65c1ee4b664e647c771e0aa7c613d
Rutupatil08 <100123001+Rutupatil08@users.noreply.github.com>
2024-06-23 15:57:03 +0530

    Create LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.java

---
+++ LongestContinuousSubarrayWithAbsoluteDiffLessThanorEqualtoLimit.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+        
+        int left = 0; 
+        int maxLength = 0; 
+        
+        for (int right = 0; right < nums.length; right++) {
+            while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] <= nums[right]) {
+                maxDeque.pollLast();
+            }
+            maxDeque.addLast(right);
+
+            while (!minDeque.isEmpty() && nums[minDeque.peekLast()] >= nums[right]) {
+                minDeque.pollLast();
+            }
+            minDeque.addLast(right);
+
+            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > limit) {
+                left++;
+                if (maxDeque.peekFirst() < left) maxDeque.pollFirst();
+                if (minDeque.peekFirst() < left) minDeque.pollFirst();
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
